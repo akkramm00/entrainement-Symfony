7b861ba042a7df9c4f4988f149906baa28bd0d42
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-21 15:26:47 +0000

    add explication de fonctionnement de la méthode "render"

--- index.php
+++ index.php
@@ -19,6 +19,13 @@
  {% block body %}
     <h1> Votre numéro porte-bonheur est le {{ number }}</h1>
  {% endblock  %}
+
+    <h2>Explication de la tache</h2>
+    <p>
+      Ici , nous utilisons le template de base fourni avec Symfony. Ce fichier se trouve dans le dossier "/src/template". Par defaut , la  méthode "rende" cherchera les templates dans ce dossier. Ainsi, si un template se trouve à la racine de ce dossier, il n'est pas nécessiare de donner toute l'arborescense.
+
+      Pour préciser les paramètres que vous souhaitons passer à notre vue, la méthode render attend un tableau assocatif, contenant en clé l'indentifiant du paramùètre que nous ouhaitons utiliser dans notre vue, et en valeur la vairiable provenant de notre controller.
+    </p>
  
   </body>
 </html>
